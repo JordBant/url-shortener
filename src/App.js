ec96b5f9436779a853df6698d572be9a969fb9f4
JordBant <jaybant23@gmail.com>
2022-05-31 12:58:35 -0400

    Revert "Initial commit for url shortener"
    This reverts commit d2083d8b88dfba96f9cf677848cc83c1f3a8790f.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import Particles from "./components/Particles"
-import "./App-styles/App.css";
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
     <div className="App">
-      <Particles />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
