81081e81437579963c98eff5e1cea5c5e54ac68e
JordBant <jaybant23@gmail.com>
2022-05-31 13:08:52 -0400

    Initial commit

---
+++ src/components/Particles.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ParticlesConfig from '../config/particles-config';
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
+const Particle = () => {
+  const particlesInit = async (main) => {
+    console.log(main);
+    await loadFull(main);
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+  return (
+    <Particles
+      id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={ ParticlesConfig }
+    />
+  )
+}
+
+  export default Particle
---
+++ src/components/url-shortened/url-shortened-component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './url-shortened-styles/url-shortened-styles.css'
+
+const UrlShort = () =>  {
+  return (
+    <div> UrlShort </div>
+  )
+}
+
+export default UrlShort
