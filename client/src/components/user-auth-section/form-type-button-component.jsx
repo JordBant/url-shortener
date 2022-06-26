989c1a933950231ba38e406da248c540873b9096
JordBant <jaybant23@gmail.com>
2022-06-26 11:50:39 -0400

    More conditional mounting using switch statements.VSCode also seems to using a lot of battery life... this could be due to the particle affects it is rendering in this project

--- client/src/components/user-auth-section/form-type-button-component.jsx
+++ client/src/components/user-auth-section/form-type-button-component.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import SignUp from './signup-component' 
+import Login from './login-component' 
 
 const FormType = ({active, title, formType, activateForm}) => {
   const isActive =  active ? 'active' : '' ;
+  const mountForm = (formType) => {
+    switch (formType) {
+      case 'Create':
+        <SignUp/>
+        break;
+      case 'Login':
+        <Login/>
+        break;
+    
+      default:
+        break;
+    }
+  }
 
   return (
       <div 
         className= {`form-container ${isActive}`} 
         onClick={() => activateForm(formType)}
       >
-        {title}
+    
+      <SignUp/>
+        {
+          active
+          ? mountForm(formType)
+          : title
+  
+        }
       </div>
       )
 }
