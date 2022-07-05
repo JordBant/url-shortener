7e1d1db3d37734648a698af38124156b25737042
JordBant <jaybant23@gmail.com>
2022-07-05 19:46:16 -0400

    More toast integration with state

--- client/src/components/routes/entry-page/entry-page-component.jsx
+++ client/src/components/routes/entry-page/entry-page-component.jsx
@@ -1,9 +1,14 @@
 import Particle from '../../Particles'
 import './entry-page-styles/entry-page.css'
 import UrlInput from '../../url-section/url-input-component'
+import { useState } from 'react'
 
 const EntryPage = () => {
-  // const [inView, setInView] = useState(false)
+  const [authorized, setAuth] = useState(false)
+  if(!authorized){
+    //trigger toast one level up
+    //send css class a level down
+  }
 
   return (
     <div className="entry-page">
--- client/src/components/toast-notify/toast-notify-component.jsx
+++ client/src/components/toast-notify/toast-notify-component.jsx
@@ -2,6 +2,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import { Fragment } from 'react';
 import 'react-toastify/dist/ReactToastify.css'; 
 import './toast-notify-styles/toast-notify.css'; 
+import { containerClasses } from '@mui/system';
 
 const MyToast = () => {
   const toastConfig = { 
@@ -9,12 +10,12 @@ const MyToast = () => {
   }
 
   const notify = () => {
-    toast.error("For security assurance, you must be an registered and authenticated user to use create urShort URL.", toastConfig);
+    toast.error("For security assurances, you must be a registered and authenticated user to create urShort URL.", toastConfig);
   }
 
   return (
     <Fragment>
-      <ToastContainer autoClose = {8000} limit = {1} />
+      <ToastContainer autoClose = {5000} limit = {1} />
       <button onClick={notify}>Notify!</button>
     </Fragment>
   );
