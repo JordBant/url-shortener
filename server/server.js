16b07d014c59745894f8b38baf85f5ec1f818e78
JordBant <jaybant23@gmail.com>
2022-07-07 18:20:16 -0400

    ...

--- server/server.js
+++ server/server.js
@@ -7,6 +7,24 @@ const express = require('express')
 const app = express()
 app.use(express.json());
 
+app.post('/form/:type', (req, res) => {
+    const user = req.body.value
+    const type = req.params.type
+
+    // switch (type) {
+    //     case 'Create':
+            
+    //         break;
+
+    //     case 'Login':
+            
+    //         break;
+    // }
+
+    console.log('Got Url:', url)
+    res.json(user)
+})
+
 app.post('/shortener', (req, res) => {
     const url = req.body.value
     console.log('Got Url:', url)
