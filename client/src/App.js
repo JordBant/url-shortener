71e2d00aac13b7ef71a6c9c7b7acc54f3d3e88cf
JordBant <jaybant23@gmail.com>
2022-07-07 18:25:19 -0400

    Implementing Scroll into view via useRef

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { toast } from 'react-toastify';
-import { useState, Fragment } from 'react';
+import { useState, useRef } from 'react';
 import EntryPage from './components/routes/entry-page/entry-page-component';
 import UserAuthPage from './components/routes/user-auth-page/user-auth-page-component';
 
@@ -10,6 +10,21 @@ import MyToast from './components/toast-notify/toast-notify-component';
 const App = () => {
   // state will at some point be influenced by a browser cookie
   const [authorized, setAuthorized] = useState(false)
+  const userAuthRef = useRef()
+
+  const smoothScroll = () => {
+    !authorized && userAuthRef.current.scrollIntoView({ behavior: 'smooth' })
+  }
+  /**
+   * 
+   * Forward a ref to to the userAuth page
+   * 
+   * Create func in App.js that smooth scrolls user page if App.js has !authorized
+   * 
+   * Execute from urShort input if !authorized
+   * 
+   */
+
 
   //triggers from unauthorized input from urShort Input
   const notify = () => {
@@ -20,7 +35,10 @@ const App = () => {
   return (
     <div className="App">
       <MyToast/>
-      <EntryPage authorized = { authorized } notify = { notify } />
+      <EntryPage 
+        authorized = { authorized } 
+        smoothScroll = { smoothScroll }
+        notify = { notify } />
       <UserAuthPage/>
     </div>
   );
