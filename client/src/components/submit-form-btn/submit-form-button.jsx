6c11e8441ed7940697073676f57fe14bde73d7cc
JordBant <jaybant23@gmail.com>
2022-07-11 15:14:18 -0400

    Creating checks for empty form

--- client/src/components/submit-form-btn/submit-form-button.jsx
+++ client/src/components/submit-form-btn/submit-form-button.jsx
@@ -1,11 +1,11 @@
 import './submit-form-styles/submit-form.css'
-import { useState } from 'react'
+import { Fragment, useState } from 'react'
 
 const SubmitFormBtn = ({newUserObject: user, buttonName, uriType}) => {
 
   /**
    * 
-   * The formBtn will send the new user to the backend onClick
+   * The formBtn will send the new user to the backend onClick if it is a fully completed form
    * 
    * The response will be an updated object that has a true or false boolean depending
    * on if any tests passed or failed for the respective information.
@@ -15,14 +15,19 @@ const SubmitFormBtn = ({newUserObject: user, buttonName, uriType}) => {
    * 
    */
 
-  const [send, setSend] = useState(false)
-  const formType = send ? uriType : ''
+  const [showError, setShowError] = useState(false)
+  
+  const checkForFullForm = (candidate) => {
+    // All values in object are not empty execute postUserInfo
+  }
 
-  const postUserInfo = async (event) => {
-    event.preventDefault(event)
-    
+  const postUserInfo = async () => {    
+
+    //Set showError if this function runs at all
+    showError && setShowError(false)
+
     try {
-      const res = await fetch(`./form/:${formType}`, {
+      const res = await fetch(`./form/:${uriType}`, {
 
           method: 'POST',
           headers: {"Content-Type" : "application/json"},
@@ -37,7 +42,10 @@ const SubmitFormBtn = ({newUserObject: user, buttonName, uriType}) => {
       }  
 
   return (
-    <button onClick={ postUserInfo } className = 'submit-form-btn' > {buttonName} </button>
+    <Fragment>
+      <button onClick={ checkForFullForm(user) } className = 'submit-form-btn' > {buttonName} </button>
+      {showError && <small className='btn-error-message'> Please fill out the incomplete fields </small>}
+    </Fragment>
   )
 }
 
